2432eca2b9d0f739c59ce0f17b49cf9a9182ed7f
dellapermata51-ui <della.permata51@ui.ac.id>
2026-09-09 16:08:47 +0700

    Memperbarui requirements.txt dengan versi django 5.0

--- main/views.py
+++ main/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+
 def show_main(request):
     context = {
-        'name': 'Della Permata Prasilda',
-        'class': 'PBP A', # Bisa kamu sesuaikan
-        'npm': '2506656614', # Bebas diisi atau disesuaikan
-        'description': 'Selamat datang di website portofolio pribadiku!',
+        "name": "Della Permata Prasilda",
+        "npm": "2506656614",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswa program studi Sistem Informasi, Fakultas Ilmu Komputer, Universitas Indonesia yang tertarik "
+            "pada pengembangan perangkat lunak dan pendidikan."
+        ),
     }
-    return render(request, "main.html", context)
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Della Permata Prasilda",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
