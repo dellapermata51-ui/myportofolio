601edcc015ee2b4a8e753e62e9dbaede6f239fe9
dellapermata51-ui <della.permata51@ui.ac.id>
2026-09-13 21:55:47 +0700

    Menambahkan Education dan Skills ke dalam file view

--- main/views.py
+++ main/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-
-from main.models import Experience
+from main.models import Experience, Education, Skill
 
 
 def show_main(request):
+    experience_list = Experience.objects.all()
     context = {
         "name": "Della Permata Prasilda",
         "npm": "2506656614",
-        "study_program": "S1 Sistem Informasi",
+        "study_program": "Information System",
         "bio": (
             "Mahasiswa program studi Sistem Informasi, Fakultas Ilmu Komputer, Universitas Indonesia yang tertarik "
             "pada pengembangan perangkat lunak dan pendidikan."
@@ -22,3 +22,19 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+
+def show_education(request):
+    context = {
+        "name": "Della Permata Prasilda",
+        "education_list": Education.objects.all().order_by('-started_at'),
+    }
+    return render(request, "education.html", context)
+
+
+def show_skills(request):
+    context = {
+        "name": "Della Permata Prasilda",
+        "skill_list": Skill.objects.all(),
+    }
+    return render(request, "skills.html", context)
