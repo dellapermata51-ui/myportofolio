20002aa6347d5a3b1c1a7eef30acb9d027aa9160
dellapermata51-ui <della.permata51@ui.ac.id>
2026-09-14 21:40:36 +0700

    Menyelesaikan Tugas 2

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -55,8 +55,8 @@ ROOT_URLCONF = 'portofolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],  
-        'APP_DIRS': True,  
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
