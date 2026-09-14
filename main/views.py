7b56f2fccfaa7d346fe74d229bcffa5b82691347
dellapermata51-ui <della.permata51@ui.ac.id>
2026-09-14 21:22:17 +0700

    Menyelesaikan Tugas 2

--- main/views.py
+++ main/views.py
@@ -9,8 +9,10 @@ def show_main(request):
         "npm": "2506656614",
         "study_program": "Information System",
         "bio": (
-            "Mahasiswa program studi Sistem Informasi, Fakultas Ilmu Komputer, Universitas Indonesia yang tertarik "
-            "pada pengembangan perangkat lunak dan pendidikan."
+            "Hi, I’m Della! a Sistem Informasi student who enjoys turning curiosity into something meaningful."
+            " I’m interested in how technology, people, and ideas can come together to create useful things."
+            " From building digital projects to working on social initiatives, I’m always curious to learn, try something new, and see where it takes me."
+            " Still learning, still exploring, and probably still figuring things out, but that’s what makes the journey interesting."
         ),
     }
     return render(request, "index.html", context)
