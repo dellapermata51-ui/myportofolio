0cf14b456db120e2d3d1621308892a78e6b5df2b
dellapermata51-ui <della.permata51@ui.ac.id>
2026-09-21 05:32:28 +0700

    Menambahkan urlpatterns untuk education di urls.py

--- main/urls.py
+++ main/urls.py
@@ -11,8 +11,15 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"),
     path("education/", show_education, name="show_education"),
     path("skills/", show_skills, name="show_skills"),
+
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
     path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
+
+    path("education/", show_education, name="show_education"),
+    path("education/add/", create_education, name="create_education"),
+    path("api/education/", get_education_json, name="get_education_json"),
+    path("education/<uuid:education_id>/edit/", update_education, name="update_education"),
+    path("education/<uuid:education_id>/delete/", delete_education, name="delete_education"),
 ]
