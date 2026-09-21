53f4f459aeca1577cf852480c7efeb7f8603f1f5
dellapermata51-ui <della.permata51@ui.ac.id>
2026-09-21 14:17:18 +0700

    Menambahkan urlpatterns untuk bagian skills

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from main.views import (
-    show_main, show_experience, show_education, show_skills,
+    show_main, show_experience,
     create_project, show_projects, get_projects_json, delete_project,
-    create_education, get_education_json, update_education, delete_education,
+    create_education, update_education, delete_education, get_education_json, show_education,
+    create_skill, update_skill, delete_skill, get_skills_json, show_skills,
 )
 
 app_name = "main"
@@ -10,17 +11,24 @@ app_name = "main"
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
-    path("education/", show_education, name="show_education"),
-    path("skills/", show_skills, name="show_skills"),
 
+    # Projects
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
     path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
 
+    # Education
     path("education/", show_education, name="show_education"),
     path("education/add/", create_education, name="create_education"),
     path("api/education/", get_education_json, name="get_education_json"),
     path("education/<uuid:education_id>/edit/", update_education, name="update_education"),
     path("education/<uuid:education_id>/delete/", delete_education, name="delete_education"),
+
+    # Skills
+    path("skills/", show_skills, name="show_skills"),
+    path("skills/add/", create_skill, name="create_skill"),
+    path("api/skills/", get_skills_json, name="get_skills_json"),
+    path("skills/<uuid:skill_id>/edit/", update_skill, name="update_skill"),
+    path("skills/<uuid:skill_id>/delete/", delete_skill, name="delete_skill"),
 ]
